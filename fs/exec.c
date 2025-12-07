2d27f1f59394604eb55b2f8aba18822438770021
micfogas <micfogas@technotic.us>
2025-12-07 06:19:00 +0000

    susfs: updated to commit 317a1b8

--- fs/exec.c
+++ fs/exec.c
@@ -1884,6 +1884,7 @@ out_unmark:
 
 #ifdef CONFIG_KSU_SUSFS
 extern bool ksu_execveat_hook __read_mostly;
+extern bool susfs_is_boot_completed_triggered __read_mostly;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
@@ -1907,7 +1908,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 		goto orig_flow;
 	}
 
-	if (unlikely(ksu_execveat_hook)) {
+        if (unlikely(ksu_execveat_hook || !susfs_is_boot_completed_triggered)) {
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
