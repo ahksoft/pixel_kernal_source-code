cf0d711476312e9742251d0546d95c960a4747e8
micfogas <micfogas@technotic.us>
2026-01-17 13:30:08 +0000

    the main branch of sukisu (no susfs)

--- drivers/kernelsu/setuid_hook.h
+++ drivers/kernelsu/setuid_hook.h
@@ -1,8 +1,10 @@
-#ifndef __KSU_H_KSU_SETUID_HOOK
-#define __KSU_H_KSU_SETUID_HOOK
+#ifndef __KSU_H_KSU_CORE
+#define __KSU_H_KSU_CORE
 
 #include <linux/init.h>
 #include <linux/types.h>
+#include "apk_sign.h"
+#include <linux/thread_info.h>
 
 void ksu_setuid_hook_init(void);
 void ksu_setuid_hook_exit(void);
